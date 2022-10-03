14d96311b9074bced782b48fc30f96270b7db874
이호재 <jaeho3836@gmail.com>
2022-10-03 14:26:05 +0900

    1

---
+++ project1/3DScroll.js
@@ -0,0 +1,43 @@
+(function () {
+  const stageElem = document.querySelector('.stage');
+  const houseElem = document.querySelector('.house');
+  const barElem = document.querySelector('.progress-bar')
+  const mousePos = {
+    x: 0,
+    y: 0
+  };
+
+  let maxScrollValue;
+
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
+
+
+  window.addEventListener('scroll', () => {
+    // console.log(pageYOffset / maxScrollValue);
+    const scrollPer = pageYOffset / maxScrollValue;
+    const zMove = scrollPer * 980 - 490;
+    houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+
+    barElem.style.width = scrollPer * 100 + '%';
+  });
+
+  window.addEventListener('mousemove', (e) => {
+    // console.log(e.clientX, e.clientY);
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    stageElem.style.transform = 'rotateX(' + mousePos.y * 5 + 'deg) rotateY(' + mousePos.x * 5 + 'deg)'
+  });
+
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
+
+
+  stageElem.addEventListener('click', (e) => {
+    console.log(e.clientX / window.innerWidth * 100);
+    new Character({
+      xPos: e.clientX / window.innerWidth * 100
+    });
+  })
+})();
---
+++ project1/3DScrollCharacter.js
@@ -0,0 +1,70 @@
+function Character(info) {
+  this.mainElem = document.createElement('div');
+  this.mainElem.classList.add('character');
+  this.mainElem.innerHTML = `                
+  <div class="character-face-con character-head">'
+  <div class="character-face character-head-face face-front"></div>'
+  <div class="character-face character-head-face face-back"></div>'
+</div>
+<div class="character-face-con character-torso">'
+  <div class="character-face character-torso-face face-front"></div>'
+  <div class="character-face character-torso-face face-back"></div>'
+</div>
+<div class="character-face-con character-arm character-arm-right">
+  <div class="character-face character-arm-face face-front"></div>
+  <div class="character-face character-arm-face face-back"></div>
+</div>
+<div class="character-face-con character-arm character-arm-left">
+  <div class="character-face character-arm-face face-front"></div>
+  <div class="character-face character-arm-face face-back"></div>
+</div>
+<div class="character-face-con character-leg character-leg-right">
+  <div class="character-face character-leg-face face-front"></div>
+  <div class="character-face character-leg-face face-back"></div>
+</div>
+<div class="character-face-con character-leg character-leg-left">
+  <div class="character-face character-leg-face face-front"></div>
+  <div class="character-face character-leg-face face-back"></div>
+</div>
+</div>
+  `
+  document.querySelector('.stage').appendChild(this.mainElem);
+
+  this.mainElem.style.left = info.xPos + '%';
+  this.scrollState = false;
+  this.lastScrollTop = 0;
+  this.init();
+}
+
+Character.prototype = {
+  constructor: Character,
+  init: function () {
+    const self = this;
+
+    window.addEventListener('scroll', () => {
+
+      clearTimeout(self.scrollState);
+
+      if (!self.scrollState) {
+        self.mainElem.classList.add('running')
+      }
+
+      self.scrollState = setTimeout(() => {
+        self.scrollState = false;
+        self.mainElem.classList.remove('running');
+      }, 500);
+
+      if (self.lastScrollTop > pageYOffset) {
+        self.mainElem.setAttribute('data-direction', 'backward');
+      } else {
+        self.mainElem.setAttribute('data-direction', 'forward');
+      }
+
+      self.lastScrollTop = pageYOffset;
+    });
+
+    window.addEventListener('keydown', (e) => {
+      if (e.ke)
+    })
+  }
+}
